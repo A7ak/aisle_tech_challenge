17fa8b0e66cd8eacb49cb17faece0433a152e09a
A7 <ag7aka@gmail.com>
2023-05-25 21:52:26 +0530

    Added PhoneNumberActivity.kt

---
+++ app/src/main/java/com/example/aisletechchallenge/ui/login/PhoneNumberActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aisletechchallenge.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aisletechchallenge.R
+
+class PhoneNumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_number)
+    }
+}
