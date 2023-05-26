3e5a97c15d72afe1790f37ce91fae52bebf0a159
A7 <ag7aka@gmail.com>
2023-05-26 11:53:55 +0530

    Added navigation flow and migrated to binding

--- app/src/main/java/com/example/aisletechchallenge/ui/login/OtpActivity.kt
+++ app/src/main/java/com/example/aisletechchallenge/ui/login/OtpActivity.kt
@@ -1,11 +1,26 @@
 package com.example.aisletechchallenge.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aisletechchallenge.MainActivity
+import com.example.aisletechchallenge.databinding.ActivityOtpBinding
 
 class OtpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOtpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp)
+        binding = ActivityOtpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btContinue.setOnClickListener {
+            navigateToNotesScreen()
+        }
+    }
+
+    private fun navigateToNotesScreen() {
+        var intent = Intent(this@OtpActivity, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/aisletechchallenge/ui/login/PhoneNumberActivity.kt
+++ app/src/main/java/com/example/aisletechchallenge/ui/login/PhoneNumberActivity.kt
@@ -1,12 +1,26 @@
 package com.example.aisletechchallenge.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.aisletechchallenge.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aisletechchallenge.databinding.ActivityPhoneNumberBinding
 
 class PhoneNumberActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPhoneNumberBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_phone_number)
+        binding = ActivityPhoneNumberBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btContinue.setOnClickListener {
+            navigateToOtpScreen()
+        }
+    }
+
+    private fun navigateToOtpScreen() {
+        var intent = Intent(this@PhoneNumberActivity, OtpActivity::class.java)
+        startActivity(intent)
     }
 }
