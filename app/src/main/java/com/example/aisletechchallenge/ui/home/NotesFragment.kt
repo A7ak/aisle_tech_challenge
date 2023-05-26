4fee3e9e24d98b3ce44ca993fd3f87aa64c494ef
A7 <ag7aka@gmail.com>
2023-05-26 22:00:28 +0530

    Added toast

--- app/src/main/java/com/example/aisletechchallenge/ui/home/NotesFragment.kt
+++ app/src/main/java/com/example/aisletechchallenge/ui/home/NotesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.aisletechchallenge.databinding.FragmentNotesBinding
@@ -32,12 +33,15 @@ class NotesFragment : Fragment() {
 
         phoneNumberViewModel.notesSuccess.observe(viewLifecycleOwner) {data ->
             binding.progress.visibility = View.GONE
-            data
+            showToast("notes data received")
         }
 
         return binding.root
     }
 
+    private fun showToast(msg : String){
+        Toast.makeText(context, msg , Toast.LENGTH_LONG).show()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
